b511c5aa6b902182d10f862ea7cca4bb0f51a867
Martin <martin.marquez@allspringglobal.com>
2022-12-26 17:18:14 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App () {
         <li><a href="pie-chart.html">pie-chart.html</a></li>
         <li><a href="area-chart.html">area-chart.html</a></li>
         <li><a href="line-chart.html">line-chart.html</a></li>
-        <li><a href="bar-chart.html">bar-chart.html</a></li>
+        <li><a href="column-chart.html">column-chart.html</a></li>
       </ul>
       <noscript>You need to enable JavaScript to run this app.</noscript>
     </div>
